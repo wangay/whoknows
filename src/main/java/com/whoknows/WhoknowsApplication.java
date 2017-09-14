116043120ffc538d682e1246eedbc499f3083de2
alex wang <86322989@qq.com>
2017-09-14 14:45:23 +0800

    过滤sql注入攻击

--- src/main/java/com/whoknows/WhoknowsApplication.java
+++ src/main/java/com/whoknows/WhoknowsApplication.java
@@ -2,6 +2,7 @@ package com.whoknows;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.ViewResolver;
@@ -12,31 +13,32 @@ import org.springframework.web.servlet.view.JstlView;
 
 @SpringBootApplication
 @EnableScheduling
+@ServletComponentScan
 public class WhoknowsApplication extends WebMvcConfigurerAdapter {
 
-	//setting View resolver
-	@Bean
-	public ViewResolver getViewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setViewClass(JstlView.class);
-		resolver.setPrefix("/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
+    //setting View resolver
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-		registry.addResourceHandler("/app/**").addResourceLocations("/app/");
-		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
-		registry.addResourceHandler("/styles/**").addResourceLocations("/styles/");
-		registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
-		registry.addResourceHandler("/components/**").addResourceLocations("/components/");
-		registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/");
-		super.addResourceHandlers(registry);
-	}
+        registry.addResourceHandler("/app/**").addResourceLocations("/app/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/styles/**").addResourceLocations("/styles/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
+        registry.addResourceHandler("/components/**").addResourceLocations("/components/");
+        registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/");
+        super.addResourceHandlers(registry);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(WhoknowsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WhoknowsApplication.class, args);
+    }
 }
